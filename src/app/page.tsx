69f7840904e34d707cc0bf52f56be5d860e06997
Vitor Andrey <vitor.andreylopess@gmail.com>
2023-11-10 15:54:08 -0300

    feat: add exports for images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,27 @@
 import { Itim } from "next/font/google";
+import Image from "next/image";
 import { twMerge } from "tailwind-merge";
 
 const itim = Itim({ subsets: ["latin"], weight: ["400"] });
 
+import {
+  bear_img,
+  blocks_img,
+  cloud_img,
+  dice_img,
+  duck_img,
+  feeding_bottle,
+  hood,
+  pacifier,
+  star,
+} from "@/utils/exportImages";
+
 export default function Home() {
   return (
-    <div>
+    <main>
       <h1 className={twMerge("text-3xl", itim.className)}>
         Seja bem vindo Murillo!
       </h1>
-    </div>
+    </main>
   );
 }
---
+++ src/utils/exportImages.tsx
@@ -0,0 +1,21 @@
+import bear_img from "../../public/bear.png";
+import blocks_img from "../../public/blocks.png";
+import cloud_img from "../../public/cloud.png";
+import dice_img from "../../public/dice.png";
+import duck_img from "../../public/duck.png";
+import feeding_bottle from "../../public/feeding-bottle.png";
+import hood from "../../public/feeding-bottle.png";
+import pacifier from "../../public/pacifier.png";
+import star from "../../public/star.png";
+
+export {
+  bear_img,
+  blocks_img,
+  cloud_img,
+  dice_img,
+  duck_img,
+  feeding_bottle,
+  hood,
+  pacifier,
+  star,
+};
